a7a495b566bc22a8ceecc0a7e3f80aedb055d5db
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-10-10 21:24:35 +0200

    feat: update and delete user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,15 @@
-import { login, register } from '../controllers/user.js'
+import { login, register, updateName, updateUsername, updateEmail, updatePassword, updateProfile, remove } from '../controllers/user.js'
 import express from 'express'
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.patch('/update_name/:id', updateName)
+router.patch('/update_username/:id', updateUsername)
+router.patch('/update_email/:id', updateEmail)
+router.patch('/update_password/:id', updatePassword)
+router.patch('/update_profile/:id', updateProfile)
+router.delete('/delete/:id', remove)
 
 export default router
