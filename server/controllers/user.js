8937a3e6b27d68631f2179f6c923890685cae40b
Fernando <fescalona@immediait.com>
2023-10-22 11:40:51 +0200

    feat: add search for posts

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -347,9 +347,7 @@ export async function removeUser (req, res) {
       return res.status(404).json({ error: 'User not found' })
     }
 
-    res
-      .status(200)
-      .json({ success: true, message: 'User removed successfully' })
+    res.status(200).json({ success: true, message: 'User removed successfully' })
   } catch (error) {
     console.error('Error:', error.message)
     res.status(500).json({ error: 'Internal server error' })
@@ -366,6 +364,10 @@ export async function searchUsers (req, res) {
       ]
     })
 
+    if (!users) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
     // get all the posts for that specific user
     const usersWithPosts = await Promise.all(
       users.map(async (user) => {
@@ -373,6 +375,10 @@ export async function searchUsers (req, res) {
         return { user, posts }
       })
     )
+
+    if (!usersWithPosts) {
+      return res.status(404).json({ error: 'Post not found' })
+    }
 
     res.status(200).json(usersWithPosts)
   } catch (error) {
