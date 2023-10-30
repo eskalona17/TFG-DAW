e073035acfd1f88f18934d587b820e687fcc9c6f
Fernando <fescalona@immediait.com>
2023-10-30 14:18:03 +0100

    add styled components and reusable button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './variables.css'
 import SignUp from './pages/SignUp'
 
 export default function App() {
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from '../components/Button'
 
 export default function SignUp () {
   return (
-    <div>SignUp</div>
+    <>
+    <Button color="light-color" variant="light">Test button</Button>
+    </>
   )
 }
