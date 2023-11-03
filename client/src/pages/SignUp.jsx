d24adb91b49b8075b0e42fe52c2e8ae484f7d10b
Fernando <fescalona@immediait.com>
2023-11-03 11:46:27 +0100

    first approach login form working with validations

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import Form from "./../components/form/Form";
 
 export default function SignUp() {
-  const [isProfessional, setIsProfessional] = useState(false);
-
   return (
     <>
-      <h3>Crea tu perfil</h3>
-      <Form isProfessional={isProfessional} />
+      <Form />
     </>
   );
 }
