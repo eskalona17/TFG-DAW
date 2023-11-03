ce70b2e1b3591aa7e4d89353e2f60af66d16db97
Fernando <fescalona@immediait.com>
2023-11-03 11:46:27 +0100

    first approach login form working with validations

--- client/src/components/input/Input.jsx
+++ client/src/components/input/Input.jsx
@@ -1,30 +1,11 @@
-import React, { useState } from "react";
 import "./Input.css";
 
-export const Input = ({
-  type,
-  name,
-  error,
-  icon,
-  label
-}) => {
-
-  const [value, setValue] = useState('')
-
-  function handleChange(e){
-    setValue(e.target.value)
-  }
-
+export const Input = ({ type, label, name }) => {
   return (
     <div className="input-container">
-      {icon && <i className={icon}></i>}
-      <input className="input"
-        type={type ? type : "text"}
-        value={value} onChange={handleChange}
-        name={name}
-      />
-      <label className={value && 'filled'} htmlFor={name}>{label}</label>
-      {error && <p className="error">Input field can't be empty!</p>}
+      {/* {icon && <i className={icon}></i>} */}
+      <input type={type ? type : "text"} name={name} />
+      <label htmlFor={name}>{label}</label>
     </div>
   );
 };
