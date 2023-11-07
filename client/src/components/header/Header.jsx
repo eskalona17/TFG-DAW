79120f9ca4e85a02fc4ea5fe27a3e04b92c20ca1
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-11-07 16:45:41 +0100

    feat: add header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,18 @@
+import Styles from "./header.module.css"
+
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className={Styles.header}>
+      <div className={Styles.title_container}>
+        <h1 className={Styles.title}>Mascotas</h1>
+      </div>
+      <div className={Styles.search_container}>
+        <form>
+          <input type="text" placeholder="Buscar..." className={Styles.search} />
+          <button className={Styles.search_button}>E</button>
+        </form>
+      </div>
+    </header>
   )
 }
 
