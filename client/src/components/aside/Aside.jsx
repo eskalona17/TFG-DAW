76b2627aa98cf6c7c29060a3ffdbc1f68a8e061b
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-11-07 18:33:01 +0100

    feat: add aside styles

--- client/src/components/aside/Aside.jsx
+++ client/src/components/aside/Aside.jsx
@@ -1,6 +1,20 @@
+import Styles from './aside.module.css'
+
 const Aside = () => {
   return (
-    <aside>Aside</aside>
+    <aside className={Styles.aside}>
+      <div className={Styles.user_container}>
+        <img src="" alt="" className={Styles.user_img} />
+        <div className={Styles.user_info}>
+          <p className={Styles.user_name}>Nombre usuario</p>
+          <p className={Styles.user_username}>@usuario</p>
+        </div>
+      </div>
+      <div className={Styles.follow_container}>
+        <span className={Styles.followers}>123 seguidores</span>
+        <span className={Styles.followers}>54 seguidos</span>
+      </div>
+    </aside>
   )
 }
 
