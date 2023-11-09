7863e844b2eece4d07a7b56efcd254d5d0aac03c
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-11-09 20:42:13 +0100

    fix: add styles

--- client/src/components/newPost/NewPost.jsx
+++ client/src/components/newPost/NewPost.jsx
@@ -3,11 +3,13 @@ import Styles from './newPost.module.css'
 const NewPost = () => {
   return (
     <div className={Styles.newPost}>
-      <img src="" alt="" className={Styles.user_img} />
-      <form>
-        <input type="text" placeholder='¿Qué estás pensando?' />
-        <button>i</button>
-      </form>
+      <div className={Styles.newPost_container}>
+        <img src="" alt="" className={Styles.user_img} />
+        <form className={Styles.newPost_form}>
+          <input type="text" placeholder='¿Qué estás pensando?' className={Styles.newPost_input} />
+          <button className={Styles.newPost_button}>i</button>
+        </form>
+      </div>
     </div>
   )
 }
