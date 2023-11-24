0b8daa943ea2fb94fd19530a57b57a27fe423497
Laura <laurashauny@gmail.com>
2023-11-24 12:53:15 +0100

    newpost y filtro

--- client/src/components/newPost/NewPost.jsx
+++ client/src/components/newPost/NewPost.jsx
@@ -1,5 +1,5 @@
 import Styles from "./newPost.module.css";
-import { VscSend } from "react-icons/vsc";
+import { VscSend, VscVmRunning, VscLocation, VscBookmark  } from "react-icons/vsc";
 
 const NewPost = () => {
   const {
@@ -9,10 +9,21 @@ const NewPost = () => {
     newPost_form,
     newPost_input,
     newPost_button,
+    multimedia_Container,
+    multimedia_button,
   } = Styles;
 
   const orange_color = "#ffa07a";
   
+  const handleMultimediaClick=() => {
+    console.log("boton multimedia");
+  };
+  const handleUbicacionClick=() => {
+    console.log("boton Ubicacion");
+  };
+  const handleEtiquetaClick=() => {
+    console.log("boton Etiqueta");
+  };
   return (
     <div className={newPost}>
       <div className={newPost_container}>
@@ -27,6 +38,17 @@ const NewPost = () => {
             <VscSend color={orange_color} />
           </button>
         </form>
+      </div>
+      <div className={multimedia_Container}>
+            <button className={multimedia_button} onClick={handleMultimediaClick}>
+              <VscVmRunning />Multimedia
+            </button>
+            <button className={multimedia_button} onClick={handleUbicacionClick}>
+              <VscLocation/>Ubicación
+            </button>
+            <button className={multimedia_button} onClick={handleEtiquetaClick}>
+              <VscBookmark/>Etiqueta
+            </button>
       </div>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import NewPost from '../components/newPost/NewPost'
-import Post from '../components/Post/Post'
-
+import NewPost from '../components/newPost/NewPost';
+import SearchFilter from '../components/searchFilter/searchFilter';
+import Post from '../components/post/Post';
 function Home() {
   return (
     <main className="main">
       <NewPost />
+      <SearchFilter />
       <Post/>
     </main>
   )
