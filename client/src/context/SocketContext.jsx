c8b4dcaba9b424c4cb65ce87f971a7b37b694d65
imenendezl <imenendezl@cofares.es>
2023-11-30 16:57:02 +0100

    fix: messages and conversation styles

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react"
 import io from "socket.io-client"
-import { AuthContext } from "./AuthContext"
+import { AuthContext } from "./authContext"
 const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 const SocketContext = createContext()
