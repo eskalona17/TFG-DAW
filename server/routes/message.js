a060d0fdffcd3be3419216e07ee9854efe39d64c
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-12-05 21:55:30 +0100

    feat: send messages from explore

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,13 +1,12 @@
-import { createConversation, getConversation, getConversations, getMessages, sendMessage } from '../controllers/message.js'
+import { getConversation, getConversations, getMessages, sendMessage } from '../controllers/message.js'
 import protectRoute from '../middlewares/protectRoute.js'
 import express from 'express'
 
 const router = express.Router()
 
 router.get('/conversations', protectRoute, getConversations)
-router.get('/conversation/:id', protectRoute, getConversation)
 router.get('/:otherUserId', protectRoute, getMessages)
+router.post('/conversation', protectRoute, getConversation)
 router.post('/', protectRoute, sendMessage)
-router.post('/new', protectRoute, createConversation)
 
 export default router
