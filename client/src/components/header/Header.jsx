bde6f78bcd54d39161a89c88688adc3a80def69e
Fernando <fescalona@immediait.com>
2023-12-11 19:11:30 +0100

    add autocomplete search with profile image

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,31 +1,75 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import Styles from "./header.module.css";
-import { IoSearch } from "react-icons/io5";
-import { IoExitOutline } from "react-icons/io5";
-import { AuthContext } from "../../context/authContext";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useContext } from "react"
+import { Link } from "react-router-dom"
+import useUserImage from "../../hooks/useUserImage"
+import Styles from "./header.module.css"
+import { IoSearch } from "react-icons/io5"
+import { IoExitOutline } from "react-icons/io5"
+import { AuthContext } from "../../context/authContext"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+
+const serverImagePath =
+  import.meta.env.VITE_REACT_APP_API_URL + "/public/profilePic"
+
+const apiUrl = import.meta.env.VITE_REACT_APP_API_URL
 
 const Header = () => {
   const {
     header,
     title_container,
     logo,
+    user_img,
     search_container,
     search_form,
     search,
     search_button,
     notifications_container,
     notifications,
-  } = Styles;
+    search_results,
+  } = Styles
 
-  const { logout } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext)
+  const { userImage } = useUserImage(currentUser)
+
+  const [searchQuery, setSearchQuery] = useState("")
+  const [searchResults, setSearchResults] = useState([])
+  const navigate = useNavigate()
+
+  const { logout } = useContext(AuthContext)
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const response = await axios.get(
+          `${apiUrl}/api/users/search?q=${searchQuery}`,
+          {
+            withCredentials: true,
+          }
+        )
+        const data = response.data
+        setSearchResults(data)
+      } catch (error) {
+        console.error("Error:", error.message)
+      }
+    }
+
+    // Solo realiza la búsqueda si hay una consulta
+    if (searchQuery.trim() !== "") {
+      fetchSearchResults()
+    } else {
+      // Si la consulta está vacía, limpia los resultados
+      setSearchResults([])
+    }
+  }, [searchQuery])
 
   const handleLogout = async () => {
-    await logout();
-    navigate("/login");
-  };
+    await logout()
+    navigate("/login")
+  }
 
   return (
     <header className={header}>
@@ -40,11 +84,32 @@ const Header = () => {
       </div>
       <div className={search_container}>
         <form className={search_form}>
-          <input type="text" placeholder="Buscar..." className={search} />
-          <button className={search_button}>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className={search}
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <button type="button" className={search_button}>
             <IoSearch />
           </button>
         </form>
+
+        <div className={search_results}>
+          {searchResults.map((result) => (
+            <ul key={result.user._id}>
+              <li>
+                <img
+                  src={serverImagePath + "/" + result.user.profilePic}
+                  alt=""
+                  className={user_img}
+                />
+                <span>{result.user.username}</span>
+              </li>
+            </ul>
+          ))}
+        </div>
       </div>
       <div className={notifications_container}>
         <span className={notifications} onClick={handleLogout}>
@@ -52,7 +117,7 @@ const Header = () => {
         </span>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -399,31 +399,34 @@ export async function removeUser (req, res) {
 
 export async function searchUsers (req, res) {
   const query = req.query.q
+
   try {
     const users = await User.find({
       $or: [
-        { username: { $regex: query, $options: 'i' } }, // search for username
-        { name: { $regex: query, $options: 'i' } } // search for name
+        { username: { $regex: new RegExp(query, 'i') } },
+        { name: { $regex: new RegExp(query, 'i') } }
       ]
     })
 
-    if (!users) {
+    if (!users || users.length === 0) {
       return res.status(404).json({ message: 'User not found' })
     }
 
     // get all the posts for that specific user
-    const usersWithPosts = await Promise.all(
+    const usersWithPostsAndImage = await Promise.all(
       users.map(async (user) => {
         const posts = await Post.find({ author: user._id })
-        return { user, posts }
+        // Assuming profilePic is a field in your User model
+        const { _id, username, name, profilePic } = user
+        return { user: { _id, username, name, profilePic }, posts }
       })
     )
 
-    if (!usersWithPosts) {
+    if (!usersWithPostsAndImage) {
       return res.status(404).json({ error: 'Post not found' })
     }
 
-    res.status(200).json(usersWithPosts)
+    res.status(200).json(usersWithPostsAndImage)
   } catch (error) {
     console.error('Error:', error.message)
     res.status(500).json({ error: 'Internal server error' })
