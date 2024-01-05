6a2444fcdeab2932b926ad5abd78fc2c9302903a
Laura <laurashauny@gmail.com>
2024-01-05 13:35:04 +0100

    .prueba scroll

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,13 +1,46 @@
-import { PostContext } from '../../context/PostContext';
-import { useContext } from "react";
-
+import { useContext, useState, useEffect } from "react";
 import Styles from "./post.module.css";
 import Loader from "../loader/Loader";
 import PostItem from "../postItem/PostItem";
+import { PostContext } from '../../context/PostContext';
 
 const Post = () => {
   const { loading, currentFilter, feedPosts, filteredPostsByFilter } = useContext(PostContext);
+  const [visiblePosts, setVisiblePosts] = useState(5); // Número de publicaciones a mostrar inicialmente
+  const [isFetching, setIsFetching] = useState(false);
 
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      // El usuario ha llegado al final de la página
+      setIsFetching(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); 
+
+  useEffect(() => {
+    const fetchMorePosts = () => {
+      
+      setTimeout(() => {
+        setVisiblePosts((prevVisiblePosts) => prevVisiblePosts + 5);
+        setIsFetching(false);
+      }, 1000);
+    };
+
+    if (isFetching) {
+      fetchMorePosts();
+    }
+  }, [isFetching]);
+
+  const postsToDisplay = currentFilter === 'all' ? feedPosts : filteredPostsByFilter;
 
   return (
     <div className={Styles.post}>
@@ -15,46 +48,21 @@ const Post = () => {
         <Loader />
       ) : (
         <>
-        {
-          currentFilter === 'all' ? (
-            feedPosts.length > 0 ? (
-              feedPosts.map((post) => (
-                <PostItem key={post._id} post={post} />
-              ))
-            ) : (
-              <section className={Styles.noPosts}>
+          {postsToDisplay.length > 0 ? (
+            postsToDisplay.slice(0, visiblePosts).map((post) => (
+              <PostItem key={post._id} post={post} />
+            ))
+          ) : (
+            <section className={Styles.noPosts}>
+              {currentFilter === 'all' ? (
                 <p>No hay publicaciones</p>
-              </section>
-            )
-          ) : null
-        }
-        {
-          currentFilter === 'personal' ? (
-            filteredPostsByFilter.length > 0 ? (
-              filteredPostsByFilter.map((post) => (
-                <PostItem key={post._id} post={post} />
-              ))
-            ) : (
-              <section className={Styles.noPosts}>
+              ) : (
                 <p>No hay publicaciones con este perfil</p>
-              </section>
-            )
-          ) : null
-        }
-        {
-          currentFilter === 'profesional' ? (
-            filteredPostsByFilter.length > 0 ? (
-              filteredPostsByFilter.map((post) => (
-                <PostItem key={post._id} post={post} />
-              ))
-            ) : (
-              <section className={Styles.noPosts}>
-                <p>No hay publicaciones con este perfil</p>
-              </section>
-            )
-          ) : null
-        }
-      </>
+              )}
+            </section>
+          )}
+          {isFetching && <Loader />} {/* Muestra el loader mientras se obtienen más publicaciones */}
+        </>
       )}
     </div>
   );
