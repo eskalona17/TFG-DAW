8b62f3bd366734c33d1119cc343967a27a13e154
eskalona17 <fer.escalonso@gmail.com>
2024-01-05 21:28:11 +0100

    add new post to context

--- client/src/components/newPost/NewPost.jsx
+++ client/src/components/newPost/NewPost.jsx
@@ -14,19 +14,20 @@ const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 const NewPost = () => {
   const { currentUser } = useContext(AuthContext);
   const { userImage } = useUserImage(currentUser);
-  const { getPosts } = useContext(PostContext);
-  const [newPostContent, setNewPostContent] = useState("");
-  const [inputKey, setInputKey] = useState(Math.random().toString());
-  const [imageData, setImageData] = useState({
-    selectedImage: null,
-    imagePreview: null,
-  });
-
-  const { selectedImage } = imageData;
-
   const {
     newPost,
+    inputKey,
+    handleImageChange,
+    newPostContent,
+    setNewPostContent,
+    imageData,
+    // setImageData,
+  } = useContext(PostContext);
+
+
+  const {
     newPost_container,
+    newPost_content,
     user_img,
     multimedia_Container,
     multimedia_button,
@@ -38,72 +39,17 @@ const NewPost = () => {
     document.getElementById("fileInput").click();
   };
 
-  const handleSendClick = async () => {
-    try {
-      const formData = new FormData();
-      formData.append("content", newPostContent);
-
-      if (selectedImage) {
-        formData.append("media", selectedImage);
-      }
-
-      await axios.post(`${apiUrl}/api/posts/create`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        withCredentials: true,
-      });
-      getPosts();
-      toast.success("Post Publicado", {
-        position: "top-center",
-        autoClose: 3000,
-      });
-    } catch (error) {
-      console.error("Error al crear el nuevo post:", error.message);
-      console.error("Detalles del error:", error.response.data);
-      toast.error("Error al publicar el post", {
-        position: "top-center",
-        autoClose: 3000,
-      });
-    } finally {
-      setInputKey(Math.random().toString());
-      setImageData({ selectedImage: null, imagePreview: null });
-      setNewPostContent("");
-    }
-  };
-
-  const handleImageChange = (e) => {
-    const selectedFile = e.target.files[0];
-
-    // update preview of the image
-    const lector = new FileReader();
-    lector.onloadend = () => {
-      setImageData({
-        selectedImage: selectedFile,
-        imagePreview: lector.result,
-      });
-    };
-
-    if (selectedFile) {
-      lector.readAsDataURL(selectedFile);
-    } else {
-      setImageData({
-        selectedImage: null,
-        imagePreview: null,
-      });
-    }
-  };
 
   return (
-    <div className={newPost}>
-      <div className={newPost_container}>
+    <div className={newPost_container}>
+      <div className={newPost_content}>
         <img src={userImage} alt="" className={user_img} />
         <Input
           type="text"
           value={newPostContent}
           placeholder="¿Qué estás pensando?"
           onChange={(e) => setNewPostContent(e.target.value)}
-          onClick={handleSendClick}
+          onClick={newPost}
           className="newPost"
         />
       </div>
