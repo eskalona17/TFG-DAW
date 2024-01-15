b36dd5f9ce830c3d3c7df48833298eb67ac8ea90
Nacho Menéndez <nchmnndzlpz@gmail.com>
2024-01-15 23:30:51 +0100

    update: cors allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://instapet-three.vercel.app/'
 ]
 
 export default allowedOrigins
