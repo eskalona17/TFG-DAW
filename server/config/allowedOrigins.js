f1f3024ec77c8db03b403b8e1e3699a232d62294
Nacho Menendez <nchmnndzlpz@gmail.com>
2024-01-16 09:38:54 +0100

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  'https://instapet-three.vercel.app/'
+  'https://instapet-three.vercel.app/',
+  'https://instapet-three.vercel.app/login'
 ]
 
 export default allowedOrigins
